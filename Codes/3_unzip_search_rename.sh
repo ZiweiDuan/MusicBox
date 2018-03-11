899cc7d57f2b45fae72f9e64387fad0a76e5d4dd
ZiweiDuan <34292897+ZiweiDuan@users.noreply.github.com>
2018-03-11 09:01:07 -0700

    Unzip Search & Down Logs, Rename to Avoid Overwriting

---
+++ Codes/3_unzip_search_rename.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+for f in ./Raw/*_search.log.tar.gz
+do 
+	echo "Processing (unzip then tar extract) $f"
+	d=$(echo ${f} | awk -F "[/.]" '{print $4}')
+#	d=${f%%.*}
+#  ${variable%%pattern}   returns variable minus longest matching pattern from back-end
+#  ${variable#pattern}    returns variable minus shortest (single #) matching pattern from front-end
+	echo ${d}
+	tar -xvzf  $f && mv *.log  ./Search/${d}.log
+done
+
